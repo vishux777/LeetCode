196a7a146acca917761f9640d715d43b1bbe0019
Vishwas <151127265+vishux777@users.noreply.github.com>
2024-09-14 13:20:14 +0530

    Time: 69 ms (53.74%), Space: 12.6 MB (44.58%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        d = {}
+        for i, num in enumerate(nums):
+            t = target - num
+            if t in d:
+                return [d[t], i]
+            d[num] = i
+        return []
